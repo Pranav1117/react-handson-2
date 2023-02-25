1e12dc0b95739ae366628fdd022c6e5cac5bcd19
pranav <pranavbavaskar1@gmail.com>
2023-02-26 00:09:41 +0530

    react handson3

--- src/Component/Classcompo.js
+++ src/Component/Classcompo.js
@@ -1,20 +1,25 @@
 import {Component} from 'react';
+import Data from './Data'
 
 
 export default class ClassCompo extends  Component{
 constructor(){
     super();
     this.state={
+        clicked:false,
         name:"",
         department:"",
         rating:0,
-        data:[]
+        array:[]
     }
 }
 
 
-clickChange=(e)=>{
-this.setState({[e.target.name]:e.target.value})
+Change=(event)=>{
+this.setState({
+    [event.target.name]:event.target.value})
+//console.log(e.target.value)
+//console.log(this.name)
 
 
 }
@@ -22,49 +27,65 @@ this.setState({[e.target.name]:e.target.value})
         const saveData={
           name:this.state.name,
           department:this.state.department,
-          rating:this.state.rating
+          rating:this.state.rating,
+          clicked:this.state.clicked,
         }
 
         const arr = this.state.data;
         arr.push(saveData);
-        this.setState({data:arr})
+        this.setState({array:arr,data:arr})
+    }
+
+    toggleFormClick=()=>{
+        this.setState({clicked:!this.state.clicked})
+    }
+
+    onFormSubmit=(event)=>{
+        let temp_obj={
+            name:this.state.name,
+            department:this.state.department,
+            rating:this.state.rating
+        }
+        this.state.array.push(temp_obj);
+
+        this.setState({
+            clicked:true,
+            name:"",
+            department:"",
+            rating: event.target.rating,
+        })
     }
 
     render(){
         return(
         <div className='maindiv'>
-
+        { ! this.state.clicked ?
+        <>
          <h1 className='heading'>EMPLOYEE FEEDBACK FORM</h1>
-
-         <div className='inputdiv1'>Name:<input type='text' className='input1' name='name' placeholder='Enter Your Name'  onChange={this.clickChange}/>          </div>
+         <form>
+         <div className='inputdiv1'>Name:<input type='text' className='input1' name='name' 
+          placeholder='Enter Your Name'  onChange={this.Change}/>          </div>
          <br/>
 
-         <div className='inputdiv2'>Department :<input type='text' className='input1' name='department' placeholder='Enter Department'  onChange={this.clickChange}/></div>
+         <div className='inputdiv2'>Department :<input type='text' className='input1' 
+          name='department' placeholder='Enter Department'  onChange={this.Change}/></div>
          <br/>
          
-         <div className='inputdiv3'>Rating :<input type='number' className='input1' name='rating' placeholder='Enter Rating'  onChange={this.clickChange}/></div>
+         <div className='inputdiv3'>Rating :<input type='number' className='input1' 
+          name='rating' placeholder='Enter Rating'  onClick={this.Change}/></div>
          <br/>
 
-         <button className='btn' onClick={this.handleClick}>Submit</button>
+         <button className='btn' onClick={this.onFormSubmit}>Submit</button>
+         </form>
          
-         {this.state.data.map((item,index)=>{
-         
-         return(
-           
-            <div className='returndiv' key={index}>
-            
-            <span>Name :{item.name} |
-           Department :{item.department} |
-            Rating :{item.rating}</span> |
-            
-            </div>
-            
+         </> :
+        <Data data={this.state.array} toggleFunc={this.toggleFormClick}/>
+    }
+        </div>
+
+       
+       
         )
-         })}
-
-      
-         </div>
-
-        )
+        
     }
 }
